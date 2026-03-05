daab8879a275df6e36fd1f47daffcdb6c52fcf25
Artem Reva <artemreva79@gmail.com>
2026-03-05 17:24:47 +0300

    Add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/otus_open_lesson_march/',
   plugins: [
     react({
       babel: {
